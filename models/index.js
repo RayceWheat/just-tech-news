fad5971de1995770b6e23b4ebf11448d92579342
RayceWheat <RayceWheat@gmail.com>
2021-10-28 17:37:08 -0500

    added ablity to comment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -39,4 +40,4 @@ Vote.belongsTo(User, {
     foreignKey: 'post_id'
   });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all users
@@ -7,6 +7,7 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('==================');
     Post.findAll({
+      order: [['created_at', 'DESC']],
         attributes: [
           'id', 
           'post_url',
@@ -14,12 +15,20 @@ router.get('/', (req, res) => {
           'created_at'
           [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [[ 'created_at', 'DESC' ]],
         include: [
-            {
-                model: User,
-                attributes: ['username']
+          // include the Comment model here;
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
             }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
         ]
     })
     .then(dbPostData => res.json(dbPostData))
