2abe3eeb03c4b815df37f40ea71869be93f0ebe2
RayceWheat <RayceWheat@gmail.com>
2021-11-02 18:52:34 -0500

    hopefully this works

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.Port || 3001;
+const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.DB_SE,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on rotues
-app.use(routes);
+app.use(require('./controllers/'));
 
-// turn on connection to the db and server 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now Listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
